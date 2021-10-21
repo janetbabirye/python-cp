50e04ded3aed23e5c7df4ae1eb48383c723adc93
janetbabirye <jbabirye2020@gmail.com>
2021-10-21 10:57:14 +0300

    assignment

---
+++ assignment.py
@@ -0,0 +1,66 @@
+#NO.1
+x=8 #No.1
+
+y=2 #No.1
+
+print(x) #No.1a
+#printing x displays the value of x which is 8.
+
+print(y) #NO.1a
+#printing y displays the value of y that is 2.
+
+print(x*y) #No.1b
+# x*y multiplies the values of x and y that is 16
+
+print(x**y) #No.1c
+#x**y is x power y that is 8 power 2 and gives the answer as 64
+
+print(x/y) #No.1d
+#x/y divides x by y and the anwser is 4
+
+print(y//x) #No,1e
+#This divides y by x and it outputs 
+#the the number ignoring the decimal and the numbers after the decimal point and the result is zero.
+
+#print(x/0) #No.1f
+#This outputs an error because a number can't be divided by a zero
+
+#NO.2
+name = input("enter your name;") #No.2a
+print('Name:' + name)
+
+#No.2b
+NAME = input("enter your name;")
+
+ADDRESS = input("enter your address;")
+
+TEL = input("enter your phone number;")
+
+print(NAME, ADDRESS, int(TEL)) 
+
+#No.3a
+user_name = input("enter your name;")
+
+age = input("enter your age;")
+
+print('USERNAME:' + str(user_name))
+
+print('AGE:' + str(int(age)))
+
+#No.3b
+year = 2021
+YOB = (int(year)-int(age))
+print(YOB)
+
+#No.4
+r = input('enter the radius,')
+Pi = 3.14
+Area = (float(Pi)*int(r)**2)
+print(Area)
+
+#No.5a
+# A python script is a collection of commands in a file designed to be executed like a program.
+
+#No.5b
+#an interpreter creates the program. It links the files that is into machine code. 
+# the source codes are run line by line while running the program then outputs the program
